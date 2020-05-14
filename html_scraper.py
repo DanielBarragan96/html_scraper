9ee9ea279150069c71495dd6220c6e160e79825f
DanielBarragan96 <ie702554@iteso.mx>
2020-05-13 19:08:41 -0500

    Reduced time for searching elements by xpath. Also added game title game from web page title

--- html_scraper.py
+++ html_scraper.py
@@ -4,32 +4,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import os
 
-chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
-
-driver = webdriver.Chrome('C:\web_drivers\chromedriver.exe', options=chrome_options)
-#driver.minimize_window()
-#os.system('cls')
-
-games = [
-		"Smash",
-		"Mario Kart",
-		"Animal Crossing"
-		]
-
 links = [
-		"https://eshop-prices.com/games/1566-super-smash-bros-ultimate?currency=MXN"	,#Smash
-		"https://eshop-prices.com/games/164-mario-kart-8-deluxe?currency=MXN"			,#Mario Kart
-		"https://eshop-prices.com/games/4042-animal-crossing-new-horizons?currency=MXN"	 #Animal Crossing
-		]
+        "https://eshop-prices.com/games/1566-super-smash-bros-ultimate?currency=MXN"     ,#Smash
+        "https://eshop-prices.com/games/164-mario-kart-8-deluxe?currency=MXN"            ,#Mario Kart
+        "https://eshop-prices.com/games/4042-animal-crossing-new-horizons?currency=MXN"   #Animal Crossing
+        ]
 
 price_xpath = "/html/body/div[2]/div[1]/table/tbody/tr[1]/td[4]"
 
-for index in range(len(links)):
-	driver.get(links[index])
-	# wait 10 seconds
-	wait = WebDriverWait(driver, 10)
-	game_price = wait.until(EC.visibility_of_element_located((By.XPATH, price_xpath)))
-	print(games[index] + " = " + game_price.text)
+chrome_options = webdriver.ChromeOptions()
+
+driver = webdriver.Chrome('C:\web_drivers\chromedriver.exe', options=chrome_options)
+driver.minimize_window()
+WebDriverWait(driver, 3)
+os.system('cls')
+
+for link in links:
+    driver.get(link)
+    wait = WebDriverWait(driver, 5)
+    game_price = driver.find_element(By.XPATH, price_xpath)
+    game_title = driver.title[0:-34]
+    print(game_title + " = " + game_price.text)
 
 driver.close()
